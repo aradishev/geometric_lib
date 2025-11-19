c18d97797b19360230966cc53281863704bd5373
ArtemZolotukhin <artem2007zolot@gmail.com>
2025-11-20 00:00:09 +0300

    Добавлено описание функций для файлов circle.py и square.py

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    """Функция возвращает площадь круга по формуле pi*r**2"""
     return math.pi * r * r
-
-
 def perimeter(r):
+    """Функция возвращает периметр круга по формуле 2*pi*r"""
     return 2 * math.pi * r
 
+print(area(4))
+print(perimeter(4))
